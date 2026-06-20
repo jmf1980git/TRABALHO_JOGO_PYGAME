85a81d191fa133f583d2301558277ff74a901519
jmf1980git <jmfgithub@gmail.com>
2026-06-20 09:11:49 -0300

    Arquivo UML e convertendo para python

---
+++ code/enemy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Enemy import Enemy
+from Entity import Entity
+
+
+class Enemy(Enemy, Entity):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
