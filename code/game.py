ea544120b94e3f0ec9a4a26f498e87c7189a5d9e
jmf1980git <jmfgithub@gmail.com>
2026-06-20 09:40:38 -0300

    Criando main loop, conectando, menu  game class

--- code/game.py
+++ code/game.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import pygame as pg
+from code.menu import Menu
+
 class Game:
     def __init__(self):
-        self.windows = None
+        pg.init()
+        self.window = pg.display.set_mode(size= (600, 480))
 
     def run(self, ):
-        pass
+        """ print('Setup Start')
+        
+        print('Setup End')
+
+        print('Loop Start') """
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+        
+            # Check for all events
+            """ for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    print("Quitting...")
+                    pg.quit() # Close window
+                    quit() # End paygame """
--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import pygame as pg
+from code.game import Game
 
-print('Setup Start')
-pg.init()
-windows = pg.display.set_mode(size= (600, 480))
-print('Setup End')
-
-print('Loop Start')
-while True:
-    # Check for all events
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            print("Quitting...")
-            pg.quit() # Close window
-            quit() # End paygame
+game = Game()
+game.run()
